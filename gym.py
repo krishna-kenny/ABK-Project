319598816b70c3b20b5c0d6757ffa4c3bcec805c
krishna-kenny <krishna.kenny4@gmail.com>
2022-02-23 16:56:27 +0530

    final draft

--- gym.py
+++ gym.py
@@ -4,10 +4,11 @@ from datetime import datetime
 conn = ms.connect(
     host="localhost",
     user="root",
-    password="dpsbn"
+    password="dpsbn",
+    autocommit=True
 )
 
-cur = conn.cursor(buffered = True)
+cur = conn.cursor()
 
 cur.execute("create database if not exists gym;")
 cur.execute("use gym;")
@@ -36,7 +37,7 @@ CREATE TABLE if not exists plans(
 
 cur.execute("""
 CREATE TABLE if not exists bill(
-    bid int primary key not null auto_increment,
+    bid int primary key auto_increment,
     bdate date not null,
     buid int,
     bname varchar(50), 
@@ -44,16 +45,15 @@ CREATE TABLE if not exists bill(
 );
 """)
 
-cur.execute("""insert into plans values 
+
+cur.execute("select count(uid) from user;")
+if cur.fetchone()[0] == 0:
+    cur.execute("""insert into plans values 
     (1, "day", 1, 200),
     (2, "week", 7, 500),
     (3, "month", 30, 1500),
     (4, "year", 365, 12500)
-;""")
-
-
-cur.execute("select count(uid) from user;")
-if cur.fetchall()[0][0] == 0:
+    ;""")
     cur.execute("insert into user values (1, 'k', 'II', 4, 12500, '1234567890', 'address', '!@#$');")
 
 
@@ -78,73 +78,96 @@ def decrypt(p):
 
 #common to user and admin
 def new_account():
-    name = input("name: ")
-    password = input("password [number only]: ")
-    print("""
-    plans:
-        1] day    | 200
-        2] week   | 500
-        3] month  | 1500
-        4] year   | 12500      
+
+    try:
+     name = input("name: ")
+     password = input("password [number only]: ")
+     print("""
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
+|    plans:                                                      |
+|----------------------------------------------------------------|
+|        1] day    | 200                                         |
+|        2] week   | 500                                         |
+|        3] month  | 1500                                        |   
+|        4] year   | 12500                                       |
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 """)
-    plan = input("plan [enter number]: ")
-    cur.execute(f"select pcost from plans where pid = '{plan}'")
-    cost = cur.fetchone()[0]
-    ph = input("phone: ")
-    addr = input("address: ") 
-    cur.execute(f"insert into user values(NULL, '{name}', 'I', {plan}, {cost}, '{ph}', '{addr}', '{encrypt(password)}');")
-    print("your account has been created.")
+     plan = input("plan [enter number]: ")
+     cur.execute(f"select pcost from plans where pid = '{plan}'")
+     cost = cur.fetchone()[0]
+     ph = input("phone: ")
+     addr = input("address: ")
+     cur.execute(f"insert into user values(NULL, '{name}', 'I', {plan}, {cost}, '{ph}', '{addr}', '{encrypt(password)}');")
+     print("your account has been created.")
+
+    except:
+        print("error")
+
+    
 
 
 
 def del_account(uid):
 
-    if input("are you sure? [yes/no]: ") == "no":
-        return 0
-    else:
+    try:
+        if input("are you sure? [yes/no]: ") == "yes":
+            cur.execute(f"select account_lvl from user where uid = {uid};")
+            if cur.fetchone()[0] == "I":
+                cur.execute(f"delete from user where uid = {uid};")            
+            else:
+                acc = input("uid of account to be deleted: ")
+                cur.execute(f"delete from user where uid = {acc};")
 
-        if cur.execute(f"select account_lvl from user where uid = {uid};") == "I":
-            cur.execute(f"delete from user where uid = {uid};")            
+            print("the account is now deleted.")
+            return 0
+
         else:
-            acc = input("uid account to be deleted: ")
-            cur.execute(f"delete from user where uid = {acc};")
+            return 0
 
-        print("the account is now deleted.")
-
-    cur.fetchall()
+        
+    except:
+        print("error")
 
 
 
 def change_plan(uid):
-
-    print("""
-    plans:
-        1] day    | 200
-        2] week   | 500
-        3] month  | 1500
-        4] year   | 12500      
-""")    
-
-    if cur.execute(f"select account_lvl from user where uid = {uid};") == "I":
-        plan = input("new plan [enter number]: ")
-        cur.execute(f"update user pid = {plan} where uid = {uid};")            
-    else:
-        acc = input("uid account to be changed: ")
-        plan = input("new plan [enter number]: ")
-        cur.execute(f"update user pid = {plan} where uid = {acc};")
-            
-    print("the account is now deleted.")
-
-    cur.fetchall()
+    plans = {1:"200", 2:"500", 3:"1500", 4:"15000"}
+    try:
+        print("""
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
+|    plans:                                                      |
+|----------------------------------------------------------------|
+|        1] day    | 200                                         |
+|        2] week   | 500                                         |
+|        3] month  | 1500                                        |
+|        4] year   | 12500                                       |
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
+    """)    
+        cur.execute(f"select account_lvl from user where uid = {uid};")
+        if cur.fetchone()[0] == "I":
+            plan = input("new plan [enter number]: ")
+            p = plans[plan]
+            cur.execute(f"update user set pid = {plan}, cost = {p} where uid = {uid};")            
+        else:
+            uid = int(input("uid of account to be changed: "))
+            plan = int(input("new plan [enter number]: "))
+            print(plans, plans[plan])
+            p = plans[plan]
+            cur.execute(f"update user set pid = {plan}, cost = {p} where uid = {uid};")
+                
+        print("the account plan is now updated.")
+        
+    except:
+        print("error")
 
 
 #user only
 def bill(uid):
+    try:
 
-    if input("are you sure [yes/no]: ") == "no":
-        return 0
-    else:
 
+     if input("are you sure [yes/no]: ") == "yes":
+        
         cur.execute(f"select * from user where uid = {uid};")
         user_rec = cur.fetchone()
         
@@ -154,166 +177,174 @@ def bill(uid):
 """)
 
         if input("proceed? [yes/no]: ") == "yes":
-            cur.execute(f"insert into bill values (null, {datetime.date(datetime.now())}, {user_rec[0]}, {user_rec[1]}, {user_rec[4]});")
+            date = datetime.date(datetime.now())
+            cur.execute(f"insert into bill values (NULL, '{date}', {user_rec[0]}, '{user_rec[1]}', {user_rec[4]});")
             print("you have been billed successfully.")
         else:
             return 0
 
-
-    cur.fetchall()
+    except:
+        print("error")
           
         
 #admin only
 def display_all(uid):
-    inp = input("""
-    tables: 
-        1] user
-        2] plans
-        3] bill
+    try:
+     inp = input("""
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
+|    tables:                                                     |
+|----------------------------------------------------------------|
+|        1] user                                                 |
+|        2] plans                                                |
+|        3] bill                                                 |
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 """)
-    dct = {
+     dct = {
         "1": "user",
         "2": "plans",
         "3": "bill"
     }
-    for rec in cur.execute(f"select * from {dct[inp]};"):
-        print(rec)
+     cur.execute(f"select * from {dct[inp]};")
+     for rec in cur.fetchall():
+      print(rec)
 
-    cur.fetchall()
-
+    except:
+        print("error")
 
 
 def god_mode(uid):
-    if cur.execute(f"select account_lvl from user where uid = {uid};") == "I":
+    cur.execute(f"select account_lvl from user where uid = {uid};")
+    if cur.fetchall()[0][0] == "I":
         return 0
     else:
         command = input("enter mysql update command:\n")
-        cur.execute(f"{command}")
-        print("data updated.")
+        try:
+            cur.execute(f"{command}")
+            print("data updated.")
+        except:
+            print("error")
+        
     try:    
         print(cur.fetchall())
     except:
         pass
+    
 
 
 
 #log in
 def log_in(uid):
-    cur.execute(f"select name from user where uid = {uid};")
-    print(f"Welcome back {cur.fetchone()[0]}")
-    cur.execute(f"select account_lvl from user where uid = {uid};")
+    try:
+        cur.execute(f"select name from user where uid = {uid};")
+        print(f"Welcome back {cur.fetchone()[0]}")
+        cur.execute(f"select account_lvl from user where uid = {uid};")
 
-    if cur.fetchone()[0] == "I":
-        
-        while True:
-            b = input("""
+        if cur.fetchone()[0] == "I":
+
+            while True:
+                b = input("""
 |-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 |                           MENU                                 |
-|                                                                |
+|----------------------------------------------------------------|
 |    1] see your gym information                                 |
-|                                                                |
-|    2] change plan
-
-     3] print bill                                              |
-|                                                                |
-|    4] logout
-
-     5]delete account                                            |
+|    2] change plan                                              |
+|    3] print bill                                               |
+|    4] logout                                                   |
+|    5] delete account                                           |
 |-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 """)
-            if b == "1":
-                cur.execute(f"select * from user where uid = {uid};")
-                print(cur.fetchone())
-            elif b == "2": 
-                change_plan(uid)
-            elif b == "3":
-                bill(uid)
-            elif b == "4":
-                break
-            elif b == "5":
-                del_account(uid)
-            else:
-                print("invalid input")
+                if b == "1":
+                    cur.execute(f"select * from user where uid = {uid};")
+                    print(cur.fetchone())
+                elif b == "2": 
+                    change_plan(uid)
+                elif b == "3":
+                    bill(uid)
+                elif b == "4":
+                    break
+                elif b == "5":
+                    del_account(uid)
+                else:
+                    print("invalid input")
         
-    else:
+        else:
 
-        while True:
-            b = input("""
+            while True:
+                b = input("""
 |-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 |                           MENU                                 |
-|                                                                |
-|    1] see your gym information
-
-    2] change your plan.
-
-    3]print bill
-
-     4]see all gym members's information                                 |
-|                                                                |
-|    5] update any record  
-
-    6]delete any record                                      |
-|                                                                |
+|----------------------------------------------------------------|
+|    1] see your gym information                                 |
+|    2] change your plan.                                        |
+|    3] print bill                                               |
+|    4] see all gym members's information                        |
+|    5] update any record                                        |
+|    6] delete any record                                        |
 |    7] logout                                                   |
 |-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 """)
-            if b == "1":
-                cur.execute(f"select * from user where uid = {uid};")
-                print(cur.fetchone())
-            elif b == "2": 
-                change_plan(uid)
-            elif b == "3":
-                bill(uid)
-            elif b == "4":
-                cur.execute(f"select * from user;")
-                for line in cur.fetchall():
-                    print(line)
-            elif b == "5":
-                god_mode(uid)
-            elif b == "6":
-                del_account(uid)
-            elif b == "7":
-                break
-            else:
-                print("invalid input")
-    cur.fetchall()
-    
+                if b == "1":
+                    cur.execute(f"select * from user where uid = {uid};")
+                    print(cur.fetchone())
+                elif b == "2": 
+                    change_plan(uid)
+                elif b == "3":
+                    bill(uid)
+                elif b == "4":
+                    cur.execute(f"select * from user;")
+                    for line in cur.fetchall():
+                        print(line)
+                elif b == "5":
+                    god_mode(uid)
+                elif b == "6":
+                    del_account(uid)
+                elif b == "7":
+                    break
+                else:
+                    print("invalid input")
+        cur.fetchall()
+    except:
+
+        print('error')
 
 
 while True:
     inp = input("""
-    MENU:   
-        1] log in
-        2] new account
-        3] exit    
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
+|    MENU:                                                       |
+|----------------------------------------------------------------|
+|        1] log in                                               |
+|        2] new account                                          |
+|        3] exit                                                 |
+|-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X|
 """)
+    try:
+         if inp == "1":
+             username = input("username: ")
+             password = input("password: ")
+             cur.execute(f"select count(uid), uid from user where name = '{username}' and password = '{encrypt(password)}';")
+             rec = cur.fetchone()
+             if rec[0] == 0:
+                 print("incorrect details.")
+                 continue
+             else:
+                 log_in(rec[1])
+                
 
-    if inp == "1":
-        username = input("username: ")
-        password = input("password: ")
-        cur.execute(f"select count(uid), uid from user where name = '{username}' and password = '{encrypt(password)}';")
-        rec = cur.fetchone()
-        print(rec)
+         elif inp == "2":
+             new_account()
 
-        if rec[0] == 0:
-            print("incorrect details.")
-            continue
-        else:
-            log_in(rec[1])
-                   
+         elif inp == "3":
+             break
 
-    elif inp == "2":
-        new_account()
+         else:
+             print("invalid input")
 
-    elif inp == "3":
-        break
-
-    else:
-        print("invalid input")
-
+    except:
+        print('error')
 print("thank you")
 
 
-cur.flush()
+conn.commit()
 cur.close()
 conn.close()
